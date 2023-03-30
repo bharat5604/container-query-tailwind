ba5686f7c10fda8d8fb118bf75f0ef14dc86978d
bharat <bhushansingh696@gmail.com>
2023-03-30 13:49:26 +0530

    adding breakpoints

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className=" mx-auto shadow-lg p-8 relative resize-x flex flex-col overflow-hidden  max-w-[--max-width] min-w-[--min-width]">
+    <div className=" mx-auto shadow-lg p-8 relative resize-x flex flex-col overflow-hidden  max-w-[--max-width] min-w-[--min-width] ">
       <div className="grid gap-6">
         <div className="@container">
-          <div className="block @lg:grid @lg:grid-cols-2   gap-6">
+          <div className="grid @lg:grid @lg:grid-cols-2 gap-6">
             <div className="mx-auto max-w-2xl overflow-hidden rounded-2xl bg-white shadow-md">
-              <div className="@2xl:flex h-full">
-                <div className="@2xl:shrink-0">
+              <div className="@3xl:flex h-full">
+                <div className="@3xl:shrink-0">
                   <img
-                    className="h-40 w-full object-cover @2xl:h-full @2xl:w-40"
+                    className="h-40 w-full object-cover @3xl:h-full @3xl:w-40"
                     src="https://eveout.s3.ap-south-1.amazonaws.com/uploads/fzig9q3n3asuzrnqq22t.jpg"
                     alt=""
                   />
@@ -35,10 +35,10 @@ function App() {
               </div>
             </div>
             <div className="mx-auto max-w-2xl overflow-hidden rounded-2xl bg-white shadow-md">
-              <div className="@2xl:flex h-full">
-                <div className="@2xl:shrink-0">
+              <div className="@3xl:flex h-full">
+                <div className="@3xl:shrink-0">
                   <img
-                    className="h-40 w-full object-cover @2xl:h-full @2xl:w-40"
+                    className="h-40 w-full object-cover @3xl:h-full @3xl:w-40"
                     src="https://eveout.s3.ap-south-1.amazonaws.com/uploads/fzig9q3n3asuzrnqq22t.jpg"
                     alt=""
                   />
